3dc3add4402c2998819f00ff7e50fb1dd7438ad9
Ahmet Anıl Demiryürek <79675017+ahmetanildemiryurek@users.noreply.github.com>
2023-04-04 22:22:03 +0300

    Delete customer directory

--- customer/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rentalCarProjectUpdated.customer.repository;
-
-import com.rentalCarProjectUpdated.customer.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
-}
